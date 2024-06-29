2595d4b9fc80731bf3b1659b5814a022f0d02a79
Yogesh Sutar <yogeshsutar63@gmail.com>
2024-06-29 19:28:30 +0530

    Added Config server

---
+++ configserver/src/test/java/com/eazybank/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybank.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
